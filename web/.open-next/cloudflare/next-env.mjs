d0b1b04ac8dd9fabf7d79d0e49063f38b9eea9ba
Yorrany Martins Braga <yorranymb@gmail.com>
2026-01-21 03:40:21 -0400

    remoção do cloudflare pages

--- web/.open-next/cloudflare/next-env.mjs
+++
@@ -1,3 +0,0 @@
-export const production = {"NEXT_PUBLIC_SUPABASE_URL":"https://pmecrhlmynkpptpjiuhx.supabase.co","NEXT_PUBLIC_SUPABASE_ANON_KEY":"","NEXT_PUBLIC_API_URL":"https://vasta.pro"};
-export const development = {"NEXT_PUBLIC_SUPABASE_URL":"https://pmecrhlmynkpptpjiuhx.supabase.co","NEXT_PUBLIC_SUPABASE_ANON_KEY":"","NEXT_PUBLIC_API_URL":"https://vasta.pro"};
-export const test = {};
