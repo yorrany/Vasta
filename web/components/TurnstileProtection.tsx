ea3f5c70d77a8d7a0b49ef80e7dd5f9ec4b97828
Yorrany Martins Braga <yorranymb@gmail.com>
2026-01-27 14:28:44 -0400

    feat: add Turnstile protection component

--- web/components/TurnstileProtection.tsx
+++ web/components/TurnstileProtection.tsx
@@ -3,10 +3,6 @@
 import { useEffect, useState } from "react"
 import Script from "next/script"
 
-// Cloudflare Turnstile keys
-const TURNSTILE_TEST_KEY = '1x00000000000000000000AA' // Always passes
-const TURNSTILE_PROD_KEY = process.env.NEXT_PUBLIC_TURNSTILE_KEY || '0x4AAAAAACLyd4XoDMS56kOLRKOQfMRUUJU'
-
 export function TurnstileProtection() {
   const [isLocalhost, setIsLocalhost] = useState(true) // Default to true to prevent flash
 
@@ -23,15 +19,4 @@ export function TurnstileProtection() {
       strategy="afterInteractive"
     />
   )
-}
-
-return (
-  <>
-    <Script
-      src="https://challenges.cloudflare.com/turnstile/v0/api.js?onload=onloadTurnstileCallback"
-      strategy="afterInteractive"
-    />
-    <div ref={containerRef} className="hidden" />
-  </>
-)
 }
